e0049d2ec2361ff5e8e2b9f78738cc39296a9c5e
Guillermo A. Perez <gaperez64@gmail.com>
2024-06-01 20:45:08 +0200

    we have a learning scheme, yay, now I just need to collect data to somehow graph what the sucess probability of the learning policy is

---
+++ learn.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+
+import argparse
+
+from pomdpy.parsers import pomdp
+from pomdpy.env import Env
+from stable_baselines3 import PPO
+
+
+def learn(filename, buchi, cobuchi):
+    with open(filename) as f:
+        env = pomdp.parse(f.read())
+    env = Env(env, buchi, cobuchi)
+
+    print("Start learning")
+    model = PPO("MlpPolicy", env, verbose=1)
+    model.learn(total_timesteps=10_000)
+    print("Learning done!")
+
+    vec_env = model.get_env()
+    obs = vec_env.reset()
+    print("Start simulating")
+    for i in range(1000):
+        action, _states = model.predict(obs, deterministic=True)
+        obs, reward, done, info = vec_env.step(action)
+        # VecEnv resets automatically
+        # if done:
+        #   obs = env.reset()
+    print("Simulation done!")
+
+    env.close()
+
+
+# Set up the parse arguments
+parser = argparse.ArgumentParser(
+                    prog="learn.py",
+                    description="Learns for the (PO)MDP")
+parser.add_argument("filename",            # positional argument
+                    help="POMDP filename")
+parser.add_argument('-1', '--cobuchi',     # list of targets
+                    nargs='+',
+                    help='List of priority-1 states',
+                    required=False,
+                    default=[])
+parser.add_argument('-2', '--buchi',       # list of targets
+                    nargs='+',
+                    help='List of priority-2 states',
+                    required=False,
+                    default=[])
+if __name__ == "__main__":
+    args = parser.parse_args()
+    print(f"file = {args.filename}, buchi = {args.buchi}, cobuchi = {args.cobuchi}")
+    learn(args.filename, args.buchi, args.cobuchi)
+    exit(0)
