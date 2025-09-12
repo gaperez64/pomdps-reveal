0528fa44a0180c1dd8dda70d302232db0676517e
Guillermo A. Perez <gaperez64@gmail.com>
2025-09-12 14:57:15 +0200

    the tests pass, though some of them depend on spot so it makes it hard to do a CI setup

--- pomdpy/tests/test_mec.py
+++ pomdpy/tests/test_mec.py
@@ -6,8 +6,16 @@ from pomdpy.beliefsuppaut import BeliefSuppAut
 from pomdpy.parsers import pomdp
 
 
+labd_examples = ["kaspers.pomdp",
+                 "pierres.pomdp",
+                 "tiger.pomdp",
+                 "revealing-tiger.pomdp"]
+
+
 def test_mec():
     for example in next(os.walk("examples"), (None, None, []))[2]:
+        if example not in labd_examples:
+            continue
         with open(os.path.join("examples", example), "r") as f:
             with open(os.path.join("pomdpy/tests/mec", example), "rb") as d:
                 env = pomdp.parse(f.read())
@@ -20,6 +28,8 @@ def test_mec():
 
 def test_priorities():
     for example in next(os.walk("examples"), (None, None, []))[2]:
+        if example not in labd_examples:
+            continue
         with open(os.path.join("examples", example), "r") as f:
             with open(os.path.join("pomdpy/tests/mec", example), "rb") as d:
                 env = pomdp.parse(f.read())
